18a8bb04460541c67102634e94e59678905d9626
Manuelo247 <83720461+Manuelo247@users.noreply.github.com>
2022-02-26 14:42:36 -0600

    comments of Multiplexor

--- Multiplexor/Multiplexor.v
+++ Multiplexor/Multiplexor.v
@@ -1,14 +1,28 @@
+//Codigo de un multiplexor de 2 a 1
+
 module Multiplexor(
+	
+	//Entradas de valores del multiplexor
 	input[1:0] entradas,
+	
+	//Entrada de seleccion de multiplexor
 	input seleccion,
+	
+	//Salida del multiplexor
 	output reg salida
 );
 
 always@*
 begin
+	
 	if (seleccion == 1)
+	
+		//La salida sera lo que este en la entrada con posicion 1
 		salida = entradas[1];	
+	
 	else
+	
+		//La salida sera lo que este en la entrada con posicion 0
 		salida = entradas[0];
 		
 end 
