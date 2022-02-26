8897f10be90f349b93474b119e98bbadb4e2fb69
Manuelo247 <83720461+Manuelo247@users.noreply.github.com>
2022-02-25 19:16:00 -0600

    pin planner

---
+++ PinPlanner/PinPlanner.tcl
@@ -0,0 +1,92 @@
+# Copyright (C) 2020  Intel Corporation. All rights reserved.
+# Your use of Intel Corporation's design tools, logic functions 
+# and other software and tools, and any partner logic 
+# functions, and any output files from any of the foregoing 
+# (including device programming or simulation files), and any 
+# associated documentation or information are expressly subject 
+# to the terms and conditions of the Intel Program License 
+# Subscription Agreement, the Intel Quartus Prime License Agreement,
+# the Intel FPGA IP License Agreement, or other applicable license
+# agreement, including, without limitation, that your use is for
+# the sole purpose of programming logic devices manufactured by
+# Intel and sold by Intel or its authorized distributors.  Please
+# refer to the applicable agreement for further details, at
+# https://fpgasoftware.intel.com/eula.
+
+# Quartus Prime Version 20.1.1 Build 720 11/11/2020 SJ Lite Edition
+# File: C:\Practicas\Verilog_Labs\PinPlanner\PinPlanner.tcl
+# Generated on: Fri Feb 25 19:12:52 2022
+
+package require ::quartus::project
+
+set_location_assignment PIN_B8 -to KEY0
+set_location_assignment PIN_A7 -to KEY1
+set_location_assignment PIN_A8 -to LEDR0
+set_location_assignment PIN_A9 -to LEDR1
+set_location_assignment PIN_A10 -to LEDR2
+set_location_assignment PIN_B10 -to LEDR3
+set_location_assignment PIN_D13 -to LEDR4
+set_location_assignment PIN_C13 -to LEDR5
+set_location_assignment PIN_E14 -to LEDR6
+set_location_assignment PIN_D14 -to LEDR7
+set_location_assignment PIN_A11 -to LEDR8
+set_location_assignment PIN_B11 -to LEDR9
+set_location_assignment PIN_P11 -to clk
+set_location_assignment PIN_C14 -to display_0[0]
+set_location_assignment PIN_E15 -to display_0[1]
+set_location_assignment PIN_C15 -to display_0[2]
+set_location_assignment PIN_C16 -to display_0[3]
+set_location_assignment PIN_E16 -to display_0[4]
+set_location_assignment PIN_D17 -to display_0[5]
+set_location_assignment PIN_C17 -to display_0[6]
+set_location_assignment PIN_D15 -to display_0[7]
+set_location_assignment PIN_C18 -to display_1[0]
+set_location_assignment PIN_D18 -to display_1[1]
+set_location_assignment PIN_E18 -to display_1[2]
+set_location_assignment PIN_B16 -to display_1[3]
+set_location_assignment PIN_A17 -to display_1[4]
+set_location_assignment PIN_A18 -to display_1[5]
+set_location_assignment PIN_B17 -to display_1[6]
+set_location_assignment PIN_A16 -to display_1[7]
+set_location_assignment PIN_B20 -to display_2[0]
+set_location_assignment PIN_A20 -to display_2[1]
+set_location_assignment PIN_B19 -to display_2[2]
+set_location_assignment PIN_A21 -to display_2[3]
+set_location_assignment PIN_B21 -to display_2[4]
+set_location_assignment PIN_C22 -to display_2[5]
+set_location_assignment PIN_B22 -to display_2[6]
+set_location_assignment PIN_A19 -to display_2[7]
+set_location_assignment PIN_F21 -to display_3[0]
+set_location_assignment PIN_E22 -to display_3[1]
+set_location_assignment PIN_E21 -to display_3[2]
+set_location_assignment PIN_C19 -to display_3[3]
+set_location_assignment PIN_C20 -to display_3[4]
+set_location_assignment PIN_D19 -to display_3[5]
+set_location_assignment PIN_E17 -to display_3[6]
+set_location_assignment PIN_D22 -to display_3[7]
+set_location_assignment PIN_F18 -to display_4[0]
+set_location_assignment PIN_E20 -to display_4[1]
+set_location_assignment PIN_E19 -to display_4[2]
+set_location_assignment PIN_J18 -to display_4[3]
+set_location_assignment PIN_H19 -to display_4[4]
+set_location_assignment PIN_F19 -to display_4[5]
+set_location_assignment PIN_F20 -to display_4[6]
+set_location_assignment PIN_F17 -to display_4[7]
+set_location_assignment PIN_J20 -to display_5[0]
+set_location_assignment PIN_K20 -to display_5[1]
+set_location_assignment PIN_L18 -to display_5[2]
+set_location_assignment PIN_N18 -to display_5[3]
+set_location_assignment PIN_M20 -to display_5[4]
+set_location_assignment PIN_N19 -to display_5[5]
+set_location_assignment PIN_N20 -to display_5[6]
+set_location_assignment PIN_L19 -to display_5[7]
+set_location_assignment PIN_C10 -to sw0
+set_location_assignment PIN_C11 -to sw1
+set_location_assignment PIN_D12 -to sw2
+set_location_assignment PIN_C12 -to sw3
+set_location_assignment PIN_A12 -to sw4
+set_location_assignment PIN_B12 -to sw5
+set_location_assignment PIN_A13 -to sw6
+set_location_assignment PIN_A14 -to sw7
+set_location_assignment PIN_B14 -to sw8
+set_location_assignment PIN_F15 -to sw9
---
+++ PinPlanner/PinPlanner.v
@@ -0,0 +1,20 @@
+module PinPlanner(
+	
+	//Switches
+	input sw0, sw1, sw2, sw3, sw4, sw5, sw6, sw7, sw8, sw9,
+	
+	//Push_button
+	input KEY0, KEY1,
+	
+	//Displays
+	input[7:0] display_0, display_1, display_2, display_3, display_4, display_5,
+	
+	//LEDs
+	input LEDR0, LEDR1, LEDR2, LEDR3, LEDR4, LEDR5, LEDR6, LEDR7, LEDR8, LEDR9,
+	
+	//miscelaneo
+	input clk
+	
+);
+
+endmodule
