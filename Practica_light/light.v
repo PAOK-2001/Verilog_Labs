4eaca7f33dffc127701fb284cf457d6349d512a6
PAOK-2001 <2001.paok@gmail.com>
2022-02-26 13:57:15 -0600

    Commented code

--- Practica_light/light.v
+++ Practica_light/light.v
@@ -1,9 +1,10 @@
+// Codigo para controlador de laser
 module light(
+	//Entradas
 	input x1,x2,
+	//Salida
 	output f
-
 );
-
+// logica para mandar un pulso de salida (F = x1 XOR x2)
 assign f = (x1 && (!x2)) || ((!x1)&& x2);
-
 endmodule
