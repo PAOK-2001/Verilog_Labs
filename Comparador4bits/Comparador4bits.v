f52f5c2e74519f42c2f029ce3d37fbdf46d4d284
Manuelo247 <83720461+Manuelo247@users.noreply.github.com>
2022-02-26 14:32:16 -0600

    comments of Comparador4bits

--- Comparador4bits/Comparador4bits.v
+++ Comparador4bits/Comparador4bits.v
@@ -1,7 +1,11 @@
+//Comparador de cadenas de 4 bits
+
 module Comparador4bits(
 
+	//declaracion de los vectores
 	input[3:0] input1, input2,
 	
+	//salidas representando que operacion logica se cumple
 	output reg [2:0] output_comparador
 
 );
@@ -14,12 +18,18 @@ always @*
 begin 
 
 	if (input1 > input2)
+		
+		//La operacion logica que se cumple es el mayor que
 		output_comparador = 3'b001;
 		
 	else if (input1 == input2)
+	
+		//La operacion logica que se cumple es el igual que
 		output_comparador = 3'b010;
 		
 	else 
+	
+		//La operacion logica que se cumple es el menor que
 		output_comparador = 3'b100;
 		
 end
