e2906135821de35921a91ef981fc926fc783e404
PAOK-2001 <2001.paok@gmail.com>
2022-03-01 08:11:06 -0600

    Added one shot module

--- UpDown_Counter/one_shot.v
+++ UpDown_Counter/one_shot.v
@@ -1,9 +1,14 @@
 module one_shot(
-
-	input button, clock,
+	input button, clk,
 	output button_state
-	
 );
+reg FlipFlop1;
+reg FlipFlop2;
 
-
-always @
+always@(posedge clk)
+begin
+	FlipFlop1 <= ~button;
+	FlipFlop2 <= FlipFlop1;
+end
+assign button_state = ~button & ~FlipFlop2;
+endmodule
