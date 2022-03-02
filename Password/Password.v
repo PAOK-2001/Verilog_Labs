9a026851f857803c5383f3f57b7ce8931bc75083
PAOK-2001 <2001.paok@gmail.com>
2022-03-02 00:25:36 -0600

    Stable version of password verifier

--- Password/Password.v
+++ Password/Password.v
@@ -1,104 +1,130 @@
 module Password(
-	input clk, rst, confirm,
-	input[3:0] inputData,
-	output reg admitted,
-	output [6:0] display1, display2, display3, display4
+	input clk, rst,
+	input      [3:0]  enable,
+	input      [3:0]  enteredPassword,
+	output reg        admitted,
+	output     [6:0]  display1,display2,display3,display4,din,
+	output reg [4:0]  statusIndicator
 );
 
-reg [3:0] value1,value2,value3,value4;
-
-parameter	idle = 0,
-				// Estados de registro de contraseña
-				R1   = 1,
-				R2   = 2,
-				R3   = 3,
-				R4   = 4;
-				// Estados de entrada de contraseña
-	
-				
-				
+parameter	idle 	      = 0,
+				k1 	      = 1,
+				k2		      = 2,
+				k3		      = 3,
+				k4		      = 4;
+						
 reg[2:0] current_state, next_state;
-reg[3:0] target[3:0];
+parameter[3:0] pwd1 = 1;
+parameter[3:0] pwd2 = 7;
+parameter[3:0] pwd3 = 1;
+parameter[3:0] pwd4 = 2;
+
+reg[3:0] value1, value2, value3, value4;
 
 always @(posedge clk or negedge rst)
 begin
 	if(~rst)
-		current_state = idle;
+		current_state <= idle;
 	else
-		current_state = next_state;
+		current_state <= next_state;
 end
 
-always @(current_state, confirm)
+always @(current_state, enable)
 begin
 	admitted = 0;
 	case(current_state)
 		idle:
 		begin
-			if(~confirm) next_state <= R1;
-			else         next_state <= idle;
+			statusIndicator[0] = 1;
+			if(enable[0] == 1) 
+			begin
+				if(enteredPassword == pwd1) 
+					begin
+					next_state = k1;
+					value1 = enteredPassword;
+					end
+				else next_state = idle;
+			end
+			else next_state = idle;
 		end
-		// Estados de registro de contraseña
-		R1:
+		
+		k1:
 		begin
-			value1      <= inputData;
-			if(~rst) next_state = idle;
-			if(~confirm)
+			statusIndicator[1] = 1;
+			if(enable[1] == 1) 
+			begin
+				if(enteredPassword == pwd2) 
 				begin
-				target[0]  <= inputData;
-				next_state <= R2;
+				next_state = k2;
+				value2 = enteredPassword;
 				end
-			else next_state <= R1;
+				else next_state = idle;
+			end
+			else next_state = k1;			
 		end
-		R2:
+		
+		k2:
 		begin
-			value2      <= inputData;
-			if(~rst) next_state = idle;
-			if(~confirm)
+			statusIndicator[2] = 1;
+			if(enable[2] == 1) 
+			begin
+				if(enteredPassword == pwd3)
 				begin
-				target[1]  <= inputData;
-				next_state <= R3;
+				next_state = k3;
+				value3 = enteredPassword;
 				end
-			else next_state <= R2;
+				else next_state = idle;
+			end
+			else next_state = k2;	
 		end
-		R3:
+		
+		k3:
 		begin
-			value3      <= inputData;
-			if(~confirm)
+			statusIndicator[3] = 1;
+			if(enable[3] == 1) 
+			begin
+				if(enteredPassword == pwd4) 
 				begin
-				target[2]  <= inputData;
-				next_state <= R4;
+				next_state = k4;
+				value4 = enteredPassword;
 				end
-			else next_state <= R3;
+				else next_state = idle;
+			end
+			else next_state = k3;	
 		end
-		R4:
+		
+		k4:
 		begin
-			value4      <= inputData;
-			if(~rst) next_state = idle;
-			if(~confirm)
-				begin
-				target[3]  <= inputData;
-				next_state <= idle;
-				end
-			else next_state <= R4;
+			statusIndicator[4] = 1;
+			admitted = 1;
+			next_state = idle;	
 		end
 	endcase
-	
 end
 
 BCD DISPLAY_SET_1(
 	.num(value1),
 	.display7Segment(display1)
 );
+
 BCD DISPLAY_SET_2(
 	.num(value2),
 	.display7Segment(display2)
 );
+
 BCD DISPLAY_SET_3(
 	.num(value3),
 	.display7Segment(display3)
 );
+
 BCD DISPLAY_SET_4(
 	.num(value4),
 	.display7Segment(display4)
 );
+
+BCD DISPLAY_SET_IN(
+	.num(enteredPassword),
+	.display7Segment(din)
+);
+
 endmodule
