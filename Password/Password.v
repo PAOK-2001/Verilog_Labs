67286e01b7d769360501dac936d76b1707b650f7
PAOK-2001 <2001.paok@gmail.com>
2022-03-02 16:56:07 -0600

    Fixed status indicator. Displays not working

--- Password/Password.v
+++ Password/Password.v
@@ -14,12 +14,12 @@ parameter	idle 	      = 0,
 				k4		      = 4;
 						
 reg[2:0] current_state, next_state;
-parameter[3:0] pwd1 = 1;
-parameter[3:0] pwd2 = 7;
-parameter[3:0] pwd3 = 1;
-parameter[3:0] pwd4 = 2;
+parameter pwd1 = 1;
+parameter pwd2 = 5;
+parameter pwd3 = 1;
+parameter pwd4 = 2;
 
-reg[3:0] value1, value2, value3, value4;
+reg[3:0] value1 = 0, value2 = 0, value3 = 0 , value4 = 0;
 
 always @(posedge clk or negedge rst)
 begin
@@ -32,16 +32,25 @@ end
 always @(current_state, enable)
 begin
 	admitted = 0;
+	value1 = 0; 
+	value2 = 0; 
+	value3 = 0;
+	value4 = 0;
+	statusIndicator[0] = 0;
+	statusIndicator[1] = 0;
+	statusIndicator[2] = 0;
+	statusIndicator[3] = 0;
+	statusIndicator[4] = 0;
 	case(current_state)
-		idle:
+	idle:
 		begin
+			value1 = enteredPassword;
 			statusIndicator[0] = 1;
 			if(enable[0] == 1) 
 			begin
 				if(enteredPassword == pwd1) 
 					begin
 					next_state = k1;
-					value1 = enteredPassword;
 					end
 				else next_state = idle;
 			end
@@ -50,13 +59,13 @@ begin
 		
 		k1:
 		begin
+			value2 = enteredPassword;
 			statusIndicator[1] = 1;
 			if(enable[1] == 1) 
 			begin
 				if(enteredPassword == pwd2) 
 				begin
 				next_state = k2;
-				value2 = enteredPassword;
 				end
 				else next_state = idle;
 			end
@@ -65,13 +74,14 @@ begin
 		
 		k2:
 		begin
+			value3 = enteredPassword;
 			statusIndicator[2] = 1;
 			if(enable[2] == 1) 
 			begin
 				if(enteredPassword == pwd3)
 				begin
-				next_state = k3;
 				value3 = enteredPassword;
+				next_state = k3;
 				end
 				else next_state = idle;
 			end
@@ -80,12 +90,12 @@ begin
 		
 		k3:
 		begin
+			next_state = k4;
 			statusIndicator[3] = 1;
 			if(enable[3] == 1) 
 			begin
 				if(enteredPassword == pwd4) 
 				begin
-				next_state = k4;
 				value4 = enteredPassword;
 				end
 				else next_state = idle;
@@ -96,8 +106,7 @@ begin
 		k4:
 		begin
 			statusIndicator[4] = 1;
-			admitted = 1;
-			next_state = idle;	
+			admitted = 1;	
 		end
 	endcase
 end
