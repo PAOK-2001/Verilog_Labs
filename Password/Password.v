3eb7d0346d4435927671fe303d5fb451dd3a5c9c
Manuelo247 <83720461+Manuelo247@users.noreply.github.com>
2022-03-02 19:45:58 -0600

    Fixed last state bug

--- Password/Password.v
+++ Password/Password.v
@@ -14,10 +14,10 @@ parameter	idle 	      = 0,
 				k4		      = 4;
 						
 reg[2:0] current_state, next_state;
-parameter pwd1 = 1;
-parameter pwd2 = 5;
-parameter pwd3 = 1;
-parameter pwd4 = 2;
+parameter pwd1 = 4'd2;
+parameter pwd2 = 4'd5;
+parameter pwd3 = 4'd2;
+parameter pwd4 = 4'd2;
 
 parameter P = 10;
 parameter A = 11;
@@ -28,7 +28,15 @@ reg[6:0] value2 = 0;
 reg[6:0] value3 = 0; 
 reg[6:0] value4 = 0;
 
-always @(posedge clk or negedge rst)
+wire clock;
+
+clk_divider DIVIDER(
+	.clk(clk),
+	.rst(rst),
+	.clk_div(clock)
+);
+
+always @(posedge clock or negedge rst)
 begin
 	if(~rst)
 		current_state <= idle;
@@ -38,16 +46,16 @@ end
 
 always @(current_state, enable)
 begin
-	admitted <= 0;
-	value1   <= 0; 
-	value2   <= 0; 
-	value3   <= 0;
-	value4   <= 0;
-	statusIndicator[0] <= 0;
-	statusIndicator[1] <= 0;
-	statusIndicator[2] <= 0;
-	statusIndicator[3] <= 0;
-	statusIndicator[4] <= 0;
+	admitted = 0;
+	value1   = 0; 
+	value2   = 0; 
+	value3   = 0;
+	value4   = 0;
+	statusIndicator[0] = 0;
+	statusIndicator[1] = 0;
+	statusIndicator[2] = 0;
+	statusIndicator[3] = 0;
+	statusIndicator[4] = 0;
 	case(current_state)
 	idle:
 		begin
@@ -56,72 +64,73 @@ begin
 			begin
 				if(enteredPassword == pwd1) 
 					begin
-					next_state <= k1;
+					next_state = k1;
 					end
-				else next_state <= idle;
+				else next_state = idle;
 			end
-			else next_state <= idle;
+			else next_state = idle;
 		end
 		
 		k1:
 		begin
-			value1 <= pwd1;
+			value1 = pwd1;
 			statusIndicator[1] = 1;
 			if(enable[1] == 1) 
 			begin
 				if(enteredPassword == pwd2) 
 				begin
-				next_state <= k2;
+				next_state = k2;
 				end
-				else next_state <= idle;
+				else next_state = idle;
 			end
-			else next_state <= k1;			
+			else next_state = k1;			
 		end
 		
 		k2:
 		begin
-			value1 <= pwd1;
-			value2 <= pwd2;
+			value1 = pwd1;
+			value2 = pwd2;
 			statusIndicator[2] = 1;
 			if(enable[2] == 1) 
 			begin
 				if(enteredPassword == pwd3)
 				begin
-				next_state <= k3;
+				next_state = k3;
 				end
 				else next_state = idle;
 			end
-			else next_state <= k2;	
+			else next_state = k2;	
 		end
 		
 		k3:
 		begin
-			value1 <= pwd1;
-			value2 <= pwd2;
-			value3 <= pwd3;
-			value4 <= enteredPassword;
+			value1 = pwd1;
+			value2 = pwd2;
+			value3 = pwd3;
+			value4 = enteredPassword;
 			statusIndicator[3] = 1;
 			if(enable[3] == 1) 
 			begin
 				if(enteredPassword == pwd4) 
 				begin
-				next_state = k4;
+					next_state = k4;
 				end
-				else next_state <= idle;
+				else next_state = idle;
 			end
-			else next_state <= k3;	
+			else next_state = k3;	
 		end
 		
 		k4:
 		begin
-		/*
-			value1 = 7'b0001010;
-			value2 = 7'b0001011;
-			value3 = 5;
-			value4 = 5;
-		*/
-			statusIndicator[4] <= 1;
-			admitted <= 1;			
+			statusIndicator[4] = 1;
+			admitted = 1;
+			value1 = P;
+			next_state = k4;
+		end
+		
+		default:
+		begin
+			next_state = idle;
 		end
 	endcase
 end
