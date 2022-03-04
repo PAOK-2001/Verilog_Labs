12cce6de4e7c5c6a794bc81c9e8623a01c019781
PAOK-2001 <2001.paok@gmail.com>
2022-03-04 08:45:49 -0600

    Added gumnut module and operation chooser logic

--- Gumnut-ALU/ALU_Gumnut.v
+++ Gumnut-ALU/ALU_Gumnut.v
@@ -27,6 +27,47 @@ module gumnut(
 	 input         int_req,
 	 output        int_ack );
 	 
+	 
+
+parameter debug = 1'b0;
+
+localparam [2:0] alu_fn_add  = 3'b000;
+localparam [2:0] alu_fn_addc = 3'b001;
+localparam [2:0] alu_fn_sub  = 3'b010;
+localparam [2:0] alu_fn_subc = 3'b011;
+localparam [2:0] alu_fn_and  = 3'b100;
+localparam [2:0] alu_fn_or   = 3'b101;
+localparam [2:0] alu_fn_xor  = 3'b110;
+localparam [2:0] alu_fn_mask = 3'b111;
+
+localparam [1:0] shift_fn_shl = 2'b00;
+localparam [1:0] shift_fn_shr = 2'b01;
+localparam [1:0] shift_fn_rol = 2'b10;
+localparam [1:0] shift_fn_ror = 2'b11;
+
+localparam [1:0] mem_fn_ldm = 2'b00;
+localparam [1:0] mem_fn_stm = 2'b01;
+localparam [1:0] mem_fn_inp = 2'b10;
+localparam [1:0] mem_fn_out = 2'b11;
+
+localparam [1:0] branch_fn_bz  = 2'b00;
+localparam [1:0] branch_fn_bnz = 2'b01;
+localparam [1:0] branch_fn_bc  = 2'b10;
+localparam [1:0] branch_fn_bnc = 2'b11;
+
+localparam [0:0] jump_fn_jmp = 1'b0;
+localparam [0:0] jump_fn_jsb = 1'b1;
+
+localparam [2:0] misc_fn_ret  	  = 3'b000;
+localparam [2:0] misc_fn_reti 	  = 3'b001;
+localparam [2:0] misc_fn_enai 	  = 3'b010;
+localparam [2:0] misc_fn_disi 	  = 3'b011;
+localparam [2:0] misc_fn_wait 	  = 3'b100;
+localparam [2:0] misc_fn_stby 	  = 3'b101;
+localparam [2:0] misc_fn_undef_6 = 3'b110;
+localparam [2:0] misc_fn_undef_7 = 3'b111;
+
+// Logic to assign operation given the first four bits of the instruction	 
 always @*  // ALU
     if (IR_decode_alu_reg || IR_decode_alu_immed || IR_decode_mem) begin
       if (IR_decode_alu_reg) begin 
