a0ee96cff445ad2459af75b2f5758ccba0e48daf
PAOK-2001 <2001.paok@gmail.com>
2022-03-09 17:53:02 -0600

    Used one shot

--- Debouncer/debouncer.v
+++ Debouncer/debouncer.v
@@ -1,39 +1,14 @@
 module debouncer(
 	input button, clk,
-	output reg button_state = 0,
-	output button_down, button_up
+	output button_state
 );
-
-reg button_sync0, button_sync1;
-
-always@(posedge clk)
-begin
-	button_sync0 <= button;
-end
+reg FlipFlop1;
+reg FlipFlop2;
 
 always@(posedge clk)
 begin
-	button_sync1 <=  button_sync0;
+	FlipFlop1 <= ~button;
+	FlipFlop2 <= FlipFlop1;
 end
-
-reg[8:0] count;
-wire idle = (button_state==button_sync1);
-wire max  = &count;
-
-always@(posedge clk)
-begin
-	if(idle)
-		count = 0;
-	else
-		begin
-		count = count + 8'b1;
-		if(max)
-			button_state = ~button_state;
-		end
-end
-// Variables Auxiliares	
-assign button_down = ~idle & max & ~button_state;
-assign button_up   = ~idle & max & button_state;
-
+assign button_state = ~button & ~FlipFlop2;
 endmodule
-
--- Debouncer/simulation/modelsim/debouncer_run_msim_rtl_verilog.do.bak11
+++ Debouncer/simulation/modelsim/debouncer_run_msim_rtl_verilog.do.bak11
@@ -5,9 +5,9 @@ if {[file exists rtl_work]} {
 vlib rtl_work
 vmap work rtl_work
 
-vlog -vlog01compat -work work +incdir+C:/Practicas/Debouncer {C:/Practicas/Debouncer/debouncer.v}
+vlog -vlog01compat -work work +incdir+/home/paok/Documents/Verilog_Labs/Debouncer {/home/paok/Documents/Verilog_Labs/Debouncer/debouncer.v}
 
-vlog -vlog01compat -work work +incdir+C:/Practicas/Debouncer {C:/Practicas/Debouncer/debouncer_tb.v}
+vlog -vlog01compat -work work +incdir+/home/paok/Documents/Verilog_Labs/Debouncer {/home/paok/Documents/Verilog_Labs/Debouncer/debouncer_tb.v}
 
 vsim -t 1ps -L altera_ver -L lpm_ver -L sgate_ver -L altera_mf_ver -L altera_lnsim_ver -L fiftyfivenm_ver -L rtl_work -L work -voptargs="+acc"  debouncer_tb
 
